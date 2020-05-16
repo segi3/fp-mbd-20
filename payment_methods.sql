1401bef5e06f7cb473ec27089183d2d48977d452
ilhambayhaqi <57692117+ilhambayhaqi@users.noreply.github.com>
2020-05-16 11:12:24 +0700

    Update payment_methods.sql

--- payment_methods.sql
+++ payment_methods.sql
@@ -1,4 +1,15 @@
 -- table payment_methods
 -- count
 
-insert into payment_methods values ('1', 'bayar onlen');
+insert into payment_methods values ('1', 'Bank Transfer');
+insert into payment_methods values ('2', 'Dana');
+insert into payment_methods values ('3', 'Indomaret');
+insert into payment_methods values ('4', 'Alfamart');
+insert into payment_methods values ('5', 'Kredivo');
+insert into payment_methods values ('6', 'Visa');
+insert into payment_methods values ('7', 'Mastercard');
+insert into payment_methods values ('8', 'Paypal');
+insert into payment_methods values ('9', 'JCB');
+insert into payment_methods values ('10', 'American Express');
+insert into payment_methods values ('11', 'Discover');
+insert into payment_methods values ('12', 'UnionPay');
